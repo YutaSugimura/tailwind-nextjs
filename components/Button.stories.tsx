3bb78963460c95c66219012e0b44812fb48fad05
YutaSugimura <nextc3m@gmail.com>
2020-12-27 14:51:39 +0900

    Add ESLint, Prettier

--- components/Button.stories.tsx
+++ components/Button.stories.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Story, Meta } from '@storybook/react/types-6-0';
-import { Button, Props } from './Button';
+import React from 'react'
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { Button, Props } from './Button'
 
 export default {
   title: 'Example/Button',
   component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' }
+    backgroundColor: { control: 'color' },
   },
-} as Meta;
+} as Meta
 
-const Template: Story<Props> = (args) => <Button {...args} />;
+const Template: Story<Props> = (args) => <Button {...args} />
 
-export const Primary = Template.bind({});
+export const Primary = Template.bind({})
 Primary.args = {
   label: 'Button',
-};
+}
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,18 +1,14 @@
-import React from 'react';
+import React from 'react'
 
 export type Props = {
-  label: string;
+  label: string
   onClick?: () => void
-};
+}
 
 export const Button: React.FC<Props> = ({ label, onClick }) => {
-  return(
-    <button
-      type="button"
-      className="text-xl font-medium text-red-600"
-      onClick={onClick}
-    >
+  return (
+    <button type="button" className="text-xl font-medium text-red-600" onClick={onClick}>
       {label}
     </button>
-  );
-};
+  )
+}
